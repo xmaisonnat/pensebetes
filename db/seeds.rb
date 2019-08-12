993acd246d5fdf0de861fec185df8b9274cfa7f8
sebastien baiges-bello <baigesbellosebastien@gmail.com>
2019-08-12 17:37:41 +0200

    adding data on seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,25 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+users = User.new(
+  name: Faker::Name.unique.name,
+  address: Faker::Address.unique.full_address,
+  email: Faker::Internet.unique.email,
+  phone_number: Faker::.unique.cell_phone
+  )
+users.save
+end
+
+10.times do
+animals = Animal.new(
+  name: Faker::Creature::Cat.unique.name,
+  description:"Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it? Do you see a little Asian child with a blank expression on his face sitting outside on a mechanical helicopter that shakes when you put quarters in it? No? Well, that's what you see at a toy store. And you must think you're in a toy store, because you're here shopping for an infant named Jeb."
+  race: Faker::Creature::Animal.unique.name
+  )
+animals.save
+end
