9f4c8260b9b2b01c3cb25611c3ac7a508a5d3f4f
Xavier Maisonnat <x.maisonnat@gmail.com>
2019-08-13 10:43:59 +0200

    modified db

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_12_163557) do
+ActiveRecord::Schema.define(version: 2019_08_13_082030) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "animals", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reservations", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
