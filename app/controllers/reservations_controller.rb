dbc71126c1c66dbb737fe8e20e890c7b3209ccc3
sebastien baiges-bello <baigesbellosebastien@gmail.com>
2019-08-13 15:15:27 +0200

    pundit integrated partially

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,17 @@
+class ReservationsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def index
+    @reservations = Reservation.all
+  end
+
+  def accept?
+    authorize @reservation
+  end
+
+  def decline?
+    authorize @reservation
+  end
+
+
+end
---
+++ app/policies/reservation_policy.rb
@@ -0,0 +1,16 @@
+class ReservationPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def accept?
+    true
+  end
+
+  def decline?
+    true
+  end
+
+end
