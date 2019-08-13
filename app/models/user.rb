0f1ece4da4e2f47113abd91897f5fe78761f9249
sebastien baiges-bello <baigesbellosebastien@gmail.com>
2019-08-13 10:26:42 +0200

    added models to users, animals and resa

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :animals
+  has_many :reservations
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :phone_number, presence: true, uniqueness: true
 end
