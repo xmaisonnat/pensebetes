ca6a384fb387d4b262faac5669328b4daf7d84f3
sebastien baiges-bello <baigesbellosebastien@gmail.com>
2019-08-13 14:59:13 +0200

    changed member routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :animals, only: [:index, :show]
+  resources :reservations do
+    member do
+      patch 'accept'
+    end
+  end
 end
