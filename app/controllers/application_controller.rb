06e559ca3d46f2600a8486c5b4ed1192423e5e21
Xavier Maisonnat <x.maisonnat@gmail.com>
2019-08-14 15:42:20 +0200

    New and create possible

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   include Pundit
 
-  after_action :verify_authorized, except: :index, unless: :skip_pundit?
+  after_action :verify_authorized, except: [:index, :new, :create], unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :address, :phone_number
+    ])
+  end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :reservations, only: :index
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :animals, only: [:index, :show]
+  resources :animals, only: [:index, :show, :new, :create]
   resources :reservations do
     member do
       patch 'accept'
