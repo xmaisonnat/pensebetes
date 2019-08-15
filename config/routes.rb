01de8e093e4ff88fd0a3902691d46cce3f6b5e60
sebastien baiges-bello <baigesbellosebastien@gmail.com>
2019-08-15 14:41:12 +0200

    conflict fixed

--- config/routes.rb
+++ config/routes.rb
@@ -5,20 +5,9 @@ Rails.application.routes.draw do
   resources :reservations, only: :index
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
   resources :animals, only: [:index, :show, :new, :create] do
-    resources :reservations, only: [:create]
+  resources :reservations, only: [:create]
   end
-  resources :reservations do
-    member do
-      patch 'accept'
-      patch 'decline'
-    end
-    collection do
-      get 'owner_index'
-=======
-  resources :animals, only: [:index, :show, :new, :create]
-
   Rails.application.routes.draw do
     namespace :owners do
       resources :reservations do
@@ -27,7 +16,6 @@ Rails.application.routes.draw do
           patch 'decline'
         end
       end
->>>>>>> 41678b6cc9cdb37cf4d67fb5093e7f74843cf49f
     end
   end
 end
