a3b5cef15e7553e6e58508988aa19474ea9400aa
Xavier Maisonnat <x.maisonnat@gmail.com>
2019-08-15 15:03:38 +0200

    debugging

--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,11 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :animals, only: [:index, :show, :new, :create]
-
-  Rails.application.routes.draw do
-    namespace :owners do
-      resources :reservations do
-        member do
-          patch 'accept'
-          patch 'decline'
-        end
+  namespace :owners do
+    resources :reservations do
+      member do
+        patch 'accept'
+        patch 'decline'
       end
     end
   end
