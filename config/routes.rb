daf12811d26d6b3f5806f1729a56ce36da5cdf7c
sebastien baiges-bello <baigesbellosebastien@gmail.com>
2019-08-15 20:10:00 +0200

    push for all

--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,14 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :animals, only: [:index, :show, :new, :create, :destroy] do
-    resources :reservations, only: [:create]
-  end
-  namespace :owners do
-    resources :reservations do
+    resources :reservations, only: [:create] do
       member do
         patch 'accept'
         patch 'decline'
       end
     end
+  end
+  namespace :owners do
+    resources :reservations
   end
 end
