518f5f321680622a9c831319dc282412c4188393
Xavier Maisonnat <x.maisonnat@gmail.com>
2019-08-16 13:00:18 +0200

    change the policy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  # include Pundit
+  include Pundit
 
-  #after_action :verify_authorized, except: [:index, :new, :create], unless: :skip_pundit?
-  #after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+  after_action :verify_authorized, except: [:index, :new, :create], unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
   protected
 
