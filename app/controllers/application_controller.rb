faa6c355f297586d6962090540c7c522d04fe84c
Xavier Maisonnat <x.maisonnat@gmail.com>
2019-08-16 12:47:35 +0200

    comment pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   include Pundit
 
-  after_action :verify_authorized, except: [:index, :new, :create], unless: :skip_pundit?
-  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+  #after_action :verify_authorized, except: [:index, :new, :create], unless: :skip_pundit?
+  #after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
   protected
 
